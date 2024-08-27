c504f1695e85d10235948cdc6ca42f0542afd0a5
Eduardo <tkd13000@gmail.com>
2024-08-27 06:11:39 -0600

    When the hover effect is active, then a label appears

--- src/Contacts/ContactsList.jsx
+++ src/Contacts/ContactsList.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import styles from "./ContactsList.module.css";
 
 function ContactsList()
 {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () =>
+    {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () =>
+    {
+        setIsHovered(false);
+    };
+
     return(
         <main className={styles.contactContainer}>
             <header className={styles.contactContainer_title}>Contacts List</header>
-            <button className={styles.contactContainer_buttonAdd}>+</button>
+            <button className={styles.contactContainer_buttonAdd}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>+
+            {isHovered && <span className={styles.tooltip}>ADD</span>}
+            </button>
+            
         </main>
     );
 }
