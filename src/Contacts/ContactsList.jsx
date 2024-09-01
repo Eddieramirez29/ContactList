4827fc4dcdba799f30bc5cff4e008e116176346d
Eduardo <tkd13000@gmail.com>
2024-09-01 12:11:22 -0600

    Can add pictures manually

--- src/Contacts/ContactsList.jsx
+++ src/Contacts/ContactsList.jsx
@@ -3,9 +3,29 @@ import styles from "./ContactsList.module.css";
 
 function ContactsList()
 {
+    const [selectedFile, setSelectedFile] = useState(null);
+    let backgroundImage = "../../public/contactImage.png";
+    const reader = new FileReader();
+
+    const handleFileChange = (event) =>
+    {
+        const file = event.target.files[0];
+        if (file && file.type.startsWith('image/'))
+        {
+
+
+            reader.onload = (e) =>
+            {
+                setSelectedFile(e.target.result); // Guarda la Data URL en el estado
+            };
+            reader.readAsDataURL(file); // Lee el archivo y genera la Data URL
+        }
+    };
+
     const [isHovered, setIsHovered] = useState(false);
     const [add, setAdd] = useState(false);//To edit a contact
     const [save, setSave] = useState(false);//To save a contact
+
 
     const handleClickAdd = () =>
     {
@@ -35,10 +55,15 @@ function ContactsList()
                 add &&(
                     <div className={save ? styles.contact : styles.contact_hidden}>
                     <p className={styles.contact_title}>Add contact</p>
-                    <img className={styles.contactImage} src="../../public/contactImage.png" height={100} width={100} />
+
+                    <img className={selectedFile ? styles.contactImage : ""} src={selectedFile || backgroundImage} // Usa la Data URL si existe, o el fondo predeterminado 
+                    height={100} width={100}/>
+                    <input className={styles.userInformation_SearchPhoto}type="file" onChange={handleFileChange} accept="image/*"/>
                     <input className={styles.userInformation_name} type="text" placeholder={"Name"}/>
                     <input className={styles.userInformation_phoneNumber} type="text" placeholder={"Phone number"}/>
                     <input className={styles.userInformation_Email} type="text" placeholder={"E-mail"} />
+
+                    
                     <button className={styles.saveButton}
                     onClick={handleClickSave}
                     >Save</button>
